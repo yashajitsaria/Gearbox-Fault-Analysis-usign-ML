c4b65e19a0b49fd32e4005ef40ecacd97377ce30
Siddharth Gupta <siddharthdgupta@gmail.com>
2022-01-27 11:48:53 +0530

    script update

--- cleaning.py
+++ cleaning.py
@@ -1,23 +1,29 @@
-#import pandas as pd
-#import numpy as np
+import pandas as pd
+import numpy as np
 import os as os
 
 print("works")
 
-b30 = []
-blist = []
-h30 = []
-hlist = []
+broken = []
+healthy = []
 
 pth1 = 'dataset/BrokenTooth'
 pth2 = 'dataset/Healthy'
 
 for file in os.listdir(pth1):
-    blist.append(os.path.join(pth1, file))
-blist.sort()
-print(blist)
+    broken.append(os.path.join(pth1, file))
+broken.sort()
 
 for file in os.listdir(pth2):
-    hlist.append(os.path.join(pth2, file))
-hlist.sort()
-print(hlist)
+    healthy.append(os.path.join(pth2, file))
+healthy.sort()
+
+i = 0
+for file in os.listdir(pth1):
+    broken[i] = pd.read_csv(os.path.join(pth1, file))
+    i += 1
+
+j = 0
+for file in os.listdir(pth2):
+    healthy[j] = pd.read_csv(os.path.join(pth2, file))
+    j += 1
