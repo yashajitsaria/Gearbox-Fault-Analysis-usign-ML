5cbc391498d7a133707f2a49c3e7b9a9e04f81e6
Siddharth Gupta <siddharthdgupta@gmail.com>
2022-02-03 23:22:07 +0530

    aggregate dataset

--- cleaning.py
+++ cleaning.py
@@ -1,6 +1,8 @@
+from matplotlib.pyplot import axis
 import pandas as pd
 import numpy as np
 import math
+import matplotlib as plt
 import os as os
 
 # Creating empty list for broken tooth and healthy gearbox datasets
@@ -27,10 +29,19 @@ for file in os.listdir(pth2):
 # Adding load and broken/healthy information to the dataset
 for i in range(0, 10):
     load = 10*i
-    
     # failure = 1, refers the gearbox is faulty while failure = 0, refers to healthy gearbox
     broken[i]['load'] = load
     broken[i]['failure'] = 1
     healthy[i]['load'] = load
     healthy[i]['failure'] = 0
+
+# Aggregating dataset for broken and healthy
+healthy_agg = healthy[0]
+broken_agg = broken[0]
+for i in range(1,10):
+    healthy_agg = healthy_agg.append(healthy[i], ignore_index=True)
+    broken_agg = broken_agg.append(broken[i], ignore_index=True)
+
+# Agregrating total dataset
+healthy_broken_agg = pd.concat([healthy_agg, broken_agg], axis=0, ignore_index=True)
 
