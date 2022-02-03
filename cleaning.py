7a43665a835a4deeed17a9c3d8d18d671763495e
Siddharth Gupta <siddharthdgupta@gmail.com>
2022-02-03 20:40:44 +0530

    added load and failure status

--- cleaning.py
+++ cleaning.py
@@ -3,24 +3,36 @@ import numpy as np
 import math
 import os as os
 
-#creating empty list for broken tooth and healthy gearbox datasets
+# Creating empty list for broken tooth and healthy gearbox datasets
 broken = []
 healthy = []
 
-#defining the path where the datasets are stored
+# Defining the path where the datasets are stored
 pth1 = 'dataset/BrokenTooth'
 pth2 = 'dataset/Healthy'
 
-#reading the dataset as the list items using the OS library to access the files and read_csv function from pandas to read the daatasets 
+# Reading the dataset as the list items using the OS library to access the files and read_csv function from pandas to read the daatasets 
 i = 0
 for file in os.listdir(pth1):
     broken.append(pd.read_csv(os.path.join(pth1, file)))
-    print(broken[i].head())
+    #print(broken[i].head())
     i += 1
     
 j = 0
 for file in os.listdir(pth2):
     healthy.append(pd.read_csv(os.path.join(pth2, file)))
-    print(healthy[j].head())
+    #print(healthy[j].head())
     j += 1
-    
+
+# Adding load and broken/healthy information to the dataset
+for i in range(0, 10):
+    load = 10*i
+    broken[i]['load'] = load
+    broken[i]['failure'] = 1
+    print(broken[i])
+
+for i in range(0, 10):
+    load = 10*i
+    healthy[i]['load'] = load
+    healthy[i]['failure'] = 0
+    print(healthy[i])
